c5e6e56ac31dc7d6bdffd8242c94afc9c172e269
Asia Magdy <asiaelsaid665@gmail.com>
2024-09-20 21:21:56 +0300

    Add : Custom Model Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import ProductCard from "./components/ProductCard";
+import Model from "./components/UI/Model";
+import Button from "./components/UI/Button";
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <main className="container">
+        <Button onClick={() => setIsOpen(true)} className="bg-blue-500">
+          Add Product{" "}
+        </Button>
+
         <div className="p-2 m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 rounded-md">
           <ProductCard />
           <ProductCard />
@@ -14,6 +22,11 @@ const App = () => {
           <ProductCard />
           <ProductCard />
         </div>
+        <Model
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          title="ADD A NEW PRODUCT"
+        />
       </main>
     </>
   );
---
+++ src/components/UI/Model.tsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+
+import Button from "./Button";
+
+interface IProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  title?: string;
+}
+
+const Model: React.FC<IProps> = ({ isOpen, setIsOpen, title }) => {
+  return (
+    <>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+            {title && <DialogTitle className="font-bold">{title}</DialogTitle>}
+            <div className="flex gap-4">
+              <Button onClick={() => setIsOpen(false)} className="bg-blue-500">Cancel</Button>
+              <Button onClick={() => setIsOpen(false)} className="bg-gray-500">Submit</Button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default Model;
